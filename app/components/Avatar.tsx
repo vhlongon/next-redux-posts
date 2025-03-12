8582d7a88fe83fdc5810457594d559e8d5457058
vhlongon <vhlongon@gmail.com>
2025-03-12 21:58:30 +0100

    feat: add avatar component

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,47 @@
+import { cyberpunkTheme } from '../Providers/ThemeProvider';
+
+type AvatarProps = {
+  firstName: string;
+  lastName: string;
+  size?: number;
+  backgroundColor?: string;
+  textColor?: string;
+};
+
+const getInitials = (firstName: string, lastName: string): string => {
+  const firstInitial = firstName ? firstName.charAt(0).toUpperCase() : '';
+  const lastInitial = lastName ? lastName.charAt(0).toUpperCase() : '';
+  return `${firstInitial}${lastInitial}`;
+};
+
+const Avatar = ({
+  firstName,
+  lastName,
+  size = 40,
+  backgroundColor = cyberpunkTheme.colors.neonBlue,
+  textColor = cyberpunkTheme.colors.textPrimary,
+}: AvatarProps) => {
+  const initials = getInitials(firstName, lastName);
+
+  return (
+    <div
+      style={{
+        width: `${size}px`,
+        height: `${size}px`,
+        backgroundColor,
+        clipPath:
+          'polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: textColor,
+        fontSize: `${size * 0.4}px`,
+        fontWeight: 'bold',
+      }}
+    >
+      {initials}
+    </div>
+  );
+};
+
+export default Avatar;
