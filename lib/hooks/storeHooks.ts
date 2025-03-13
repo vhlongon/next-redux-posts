4a8351232310ce5ab5939aabf0c7615174e8e66c
vhlongon <vhlongon@gmail.com>
2025-03-13 22:29:15 +0100

    chore: add comment and ignore store hooks from coverage

--- lib/hooks/storeHooks.ts
+++ lib/hooks/storeHooks.ts
@@ -1,6 +1,10 @@
+// in case we need to interact with the store, we can use these hooks
+
+/* v8 ignore start */
 import { useDispatch, useSelector, useStore } from 'react-redux';
 import type { AppDispatch, AppStore, RootState } from '../store';
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
 export const useAppStore = useStore.withTypes<AppStore>();
+/* v8 ignore end */
