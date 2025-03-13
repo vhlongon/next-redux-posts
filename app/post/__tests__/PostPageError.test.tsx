f7caefba87b8a96f86ba42ae200c57d5639e7583
vhlongon <vhlongon@gmail.com>
2025-03-13 22:07:51 +0100

    chore: add  test for post page error

---
+++ app/post/__tests__/PostPageError.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@/test/utils';
+import PostPageError from '../error';
+
+vi.mock('next/navigation', () => ({
+  useParams: vi.fn().mockReturnValue({ id: '1' }),
+}));
+
+describe('PostPageError', () => {
+  test('should render the error message', () => {
+    vi.spyOn(console, 'error').mockImplementation(() => {});
+    const error = new Error('Error');
+    render(<PostPageError error={error} />);
+    expect(
+      screen.getByRole('heading', { name: 'Something went wrong!', level: 2 })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText('Error trying to fetch the post for id', {
+        exact: false,
+      })
+    ).toBeInTheDocument();
+
+    expect(console.error).toHaveBeenCalledWith(error);
+  });
+});
