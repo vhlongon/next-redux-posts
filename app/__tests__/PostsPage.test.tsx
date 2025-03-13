2e0ae8e33ad5b92f7e205ce6cf728215099b9640
vhlongon <vhlongon@gmail.com>
2025-03-13 21:38:10 +0100

    chore: add posts page test

---
+++ app/__tests__/PostsPage.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@/test/utils';
+import PostsPage from '../page';
+import { mockedPosts } from '@/test/mocks/postMock';
+
+describe('MainPage', () => {
+  test('renders the main page', async () => {
+    const searchParams = Promise.resolve({
+      limit: '20',
+      skip: '0',
+    });
+    render(await PostsPage({ searchParams }));
+
+    expect(
+      screen.getByRole('heading', { name: 'Posts', level: 2 })
+    ).toBeInTheDocument();
+
+    expect(screen.getAllByRole('listitem')).toHaveLength(mockedPosts.length);
+
+    for (const post of mockedPosts) {
+      expect(screen.getByText(post.title)).toBeInTheDocument();
+    }
+  });
+});
