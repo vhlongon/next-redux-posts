b5fe6add61807fd21c279d98c7c0e1d1ecce56b1
vhlongon <vhlongon@gmail.com>
2025-03-13 20:50:14 +0100

    chore: add test to store

---
+++ lib/utils/__tests__/store.test.ts
@@ -0,0 +1,31 @@
+import { makeStore } from '@/lib/store';
+
+describe('store', () => {
+  it('should create a store with the correct initial state and methods', () => {
+    const store = makeStore();
+    const state = store.getState();
+    expect(state).toEqual({
+      postsApi: {
+        config: {
+          focused: true,
+          invalidationBehavior: 'delayed',
+          keepUnusedDataFor: 60,
+          middlewareRegistered: false,
+          online: true,
+          reducerPath: 'postsApi',
+          refetchOnFocus: false,
+          refetchOnMountOrArgChange: false,
+          refetchOnReconnect: false,
+        },
+        mutations: {},
+        provided: {},
+        queries: {},
+        subscriptions: {},
+      },
+    });
+
+    expect(store.dispatch).toBeInstanceOf(Function);
+    expect(store.subscribe).toBeInstanceOf(Function);
+    expect(store.replaceReducer).toBeInstanceOf(Function);
+  });
+});
